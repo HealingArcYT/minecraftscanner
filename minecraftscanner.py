cd39822ba2153c9834ca8f836895cafda42f0bfa
HealingArcYT <molld807@gmail.com>
2021-02-18 08:53:11 +0100

    Initial Commit

---
+++ minecraftscanner.py
@@ -0,0 +1,29 @@
+from subprocess import Popen, PIPE
+import sys
+from mcstatus import MinecraftServer
+
+def main(argv):
+    ip = []
+    if "-version" in argv:
+        version = (lambda argv: argv[x+1] for x in range(len(argv)) if argv[x] == "-version" or argv[x] == "-v")(argv)
+    ip += [argv[x] for x in range(len(argv)) if argv[x] != "-version" or argv[x] != "-v" or argv[x-1] != "-version" or argv[x-1] != "-v"]
+
+    for i in ip:
+        proc = Popen(["nmap", "-sT", "-Pn", f"{i}", "-p-"], stdout=PIPE)
+        (out, err) = proc.communicate()
+        print(len(out)*"\b")
+        outs = []
+        for j in out.split("\n"):
+            try:
+                port = int(j[:5])
+                server = MinecraftServer.lookup(f"{i}:{port}")
+                outs += {"ipfull":f"{i}:{port}", "version":server.status().version.name}
+            except ValueError:
+                pass
+    for i in outs:
+        print(i["ipfull"], i["version"])
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
